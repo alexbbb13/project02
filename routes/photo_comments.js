4e1827dd14a2b13e135850ece2b4ef39f0a9fa5d
alexbbb13 <alex_bbb@mail.ru>
2019-06-30 00:28:07 +0300

    Heroku

--- routes/photo_comments.js
+++ routes/photo_comments.js
@@ -9,6 +9,7 @@ const pool = new Pool({connectionString: connectionString});
 router.get('/:photoId', function(req, res, next) {
 const id = req.params.photoId;
 const query = 'select username, text from comments left join users on users.id=comments.user_id where comments.photo_id=$1::int';
+const query = 'select username, text, filename from comments left join users on users.id=comments.user_id left join photos on photos.id=comments.photo_id where comments.photo_id=$1::int';
 //const query = 'SELECT * FROM relatives WHERE person_id=$1::int';
 const params = [id];
 pool.query(query,params, function(err, result) {
@@ -18,21 +19,16 @@ pool.query(query,params, function(err, result) {
         console.log(err);
     }
 
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
     const record=result.rows;
     const recordMapped = record.map(row => { 
-        var rObj = {};
+        const rObj = {};
         rObj['username'] = row.username;
         rObj['text'] = row.text;
         return rObj;
     });  
     //res.json({comments: recordMapped });
-    res.render('photo_comments', {comments: recordMapped });
-    
-    //res.json({username: [record.father_id, record.mother_id]});
-    console.log(result.rows);
-	});    
+    res.render('photo_comments', {comments: recordMapped, filename: record[0].filename });
+    });    
   
 });
 
