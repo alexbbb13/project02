a8655e615c511a89109c2c3f18798725adcc41cc
alexbbb13 <alex_bbb@mail.ru>
2019-06-30 09:24:34 +0300

    Heroku

--- routes/photo_comments.js
+++ routes/photo_comments.js
@@ -26,13 +26,37 @@ pool.query(query,params, function(err, result) {
                 rObj['text'] = row.text;
                 return rObj;});  
             //res.json({comments: recordMapped });
+            res.render('photo_comments', {comments: recordMapped, filename: record[0].filename, photoId: id });
+        } else {
+            res.render('error', {message: 'No data', error: {status : '', stack : ''}});
+        }
+    });    
+});
+
+router.post('/:photoId', function(req, res, next) {
+const id = req.params.photoId;
+const query = 'select username, text, filename from comments left join users on users.id=comments.user_id left join photos on photos.id=comments.photo_id where comments.photo_id=$1::int';
+const params = [id];
+pool.query(query,params, function(err, result) {
+    // If an error occurred...
+    if (err) {
+        console.log("Error in query: ")
+        console.log(err);
+    }
+
+    const record=result.rows;
+    if(record.length > 0) {
+            const recordMapped = record.map(row => { 
+                const rObj = {};
+                rObj['username'] = row.username;
+                rObj['text'] = row.text;
+                return rObj;});  
+            //res.json({comments: recordMapped });
             res.render('photo_comments', {comments: recordMapped, filename: record[0].filename });
         } else {
             res.render('error', {message: 'No data', error: {status : '', stack : ''}});
         }
     });    
-    
-  
 });
 
 module.exports = router;
