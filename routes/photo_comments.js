df6b2f64a6784f4d77c51cee2ad11f71b1284042
alexbbb13 <alex_bbb@mail.ru>
2019-07-01 14:17:17 +0300

    Heroku

--- routes/photo_comments.js
+++ routes/photo_comments.js
@@ -35,28 +35,17 @@ pool.query(query,params, function(err, result) {
 
 router.post('/:photoId', function(req, res, next) {
 const id = req.params.photoId;
-const query = 'select username, text, filename from comments left join users on users.id=comments.user_id left join photos on photos.id=comments.photo_id where comments.photo_id=$1::int';
-const params = [id];
+const comment = req.query.comment;
+const userId = 1; //@TODO get user id from session
+const query = 'INSERT INTO comments (user_id, photo_id, text) VALUES ($1::int,$2::int,$3::string)';
+const params = [userId, photoId, comment``];
 pool.query(query,params, function(err, result) {
     // If an error occurred...
     if (err) {
         console.log("Error in query: ")
         console.log(err);
     }
-
-    const record=result.rows;
-    if(record.length > 0) {
-            const recordMapped = record.map(row => { 
-                const rObj = {};
-                rObj['username'] = row.username;
-                rObj['text'] = row.text;
-                return rObj;});  
-            //res.json({comments: recordMapped });
-            res.render('photo_comments', {comments: recordMapped, filename: record[0].filename });
-        } else {
-            res.render('error', {message: 'No data', error: {status : '', stack : ''}});
-        }
-    });    
+    res.redirect('/' + id);
 });
 
 module.exports = router;
